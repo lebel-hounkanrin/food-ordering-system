7926b0a8c2c6331dadd233cf4ea73afc4ab93e15
neil <lebelhounkanrin@gmail.com>
2024-11-20 21:32:45 +0100

    feat: create order entities

---
+++ order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataaccess/order/entity/OrderEntity.java
@@ -0,0 +1,46 @@
+package com.food.ordering.system.order.service.dataaccess.order.entity;
+
+import com.food.ordering.system.domain.valueObject.OrderStatus;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "orders")
+@Entity
+public class OrderEntity {
+    @Id
+    private UUID id;
+    private UUID customerId;
+    private UUID restaurantId;
+    private UUID trackingId;
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+    private String failureMessages;
+
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private OrderAddressEntity address;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItemEntity> items;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderEntity that = (OrderEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
