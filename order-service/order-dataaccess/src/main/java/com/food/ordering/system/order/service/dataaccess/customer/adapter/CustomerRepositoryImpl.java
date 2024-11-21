38aa200df0e98231ec14d12dda5707862cba81da
neil <lebelhounkanrin@gmail.com>
2024-11-21 13:10:03 +0100

    feat: implement customer data access layer

---
+++ order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataaccess/customer/adapter/CustomerRepositoryImpl.java
@@ -0,0 +1,25 @@
+package com.food.ordering.system.order.service.dataaccess.customer.adapter;
+
+import com.food.ordering.system.order.service.dataaccess.customer.mapper.CustomerDataAccessMaper;
+import com.food.ordering.system.order.service.dataaccess.customer.repository.CustomerJpaRepository;
+import com.food.ordering.system.order.service.domain.entity.Customer;
+import com.food.ordering.system.order.service.domain.ports.input.CustomerRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Component
+public class CustomerRepositoryImpl implements CustomerRepository {
+    private final CustomerJpaRepository customerJpaRepository;
+    private final CustomerDataAccessMaper customerDataAccessMaper;
+    public CustomerRepositoryImpl(CustomerJpaRepository customerJpaRepository,
+                                  CustomerDataAccessMaper customerDataAccessMaper) {
+        this.customerJpaRepository = customerJpaRepository;
+        this.customerDataAccessMaper = customerDataAccessMaper;
+    }
+    @Override
+    public Optional<Customer> findCustomer(UUID customerId) {
+        return this.customerJpaRepository.findById(customerId);
+    }
+}
