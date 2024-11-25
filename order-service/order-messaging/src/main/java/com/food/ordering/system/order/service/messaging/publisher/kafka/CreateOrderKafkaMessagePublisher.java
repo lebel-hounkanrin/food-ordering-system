4e9512dc10d2ec1ee6fd7c162eac639453b843fd
neil <lebelhounkanrin@gmail.com>
2024-11-25 18:31:01 +0100

    feat: add kafka publisher

---
+++ order-service/order-messaging/src/main/java/com/food/ordering/system/order/service/messaging/publisher/kafka/CreateOrderKafkaMessagePublisher.java
@@ -0,0 +1,57 @@
+package com.food.ordering.system.order.service.messaging.publisher.kafka;
+
+import com.food.ordering.system.kafka.order.avro.model.PaymentRequestAvroModel;
+import com.food.ordering.system.kafka.producer.service.KafkaProducer;
+import com.food.ordering.system.order.service.domain.config.OrderServiceConfigData;
+import com.food.ordering.system.order.service.domain.event.OrderCreatedEvent;
+import com.food.ordering.system.order.service.domain.ports.output.message.publisher.payment.OrderCreatedPaymentRequestMessagePublisher;
+import com.food.ordering.system.order.service.messaging.mapper.OrderMessagingDataMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+@Slf4j
+@Component
+public class CreateOrderKafkaMessagePublisher implements OrderCreatedPaymentRequestMessagePublisher {
+    private final OrderMessagingDataMapper orderMessagingDataMapper;
+    private final OrderServiceConfigData orderServiceConfigData;
+    private final KafkaProducer<String, PaymentRequestAvroModel> kafkaProducer;
+
+    public CreateOrderKafkaMessagePublisher(OrderMessagingDataMapper orderMessagingDataMapper,
+                                            OrderServiceConfigData orderServiceConfigData,
+                                            KafkaProducer<String, PaymentRequestAvroModel> kafkaProducer) {
+        this.orderMessagingDataMapper = orderMessagingDataMapper;
+        this.orderServiceConfigData = orderServiceConfigData;
+        this.kafkaProducer = kafkaProducer;
+    }
+
+
+    @Override
+    public void publish(OrderCreatedEvent event) {
+        String orderId = event.getOrder().getId().getValue().toString();
+        log.info("Receive OrderCreatedEvent for order with Id: {}", orderId);
+
+        PaymentRequestAvroModel paymentRequestAvroModel = orderMessagingDataMapper.orderCreatedEventToPaymentRequestAvroModel(event);
+        kafkaProducer.send(
+                orderServiceConfigData.getPaymentRequestTopicName(),
+                orderId,
+                paymentRequestAvroModel,
+                kafkaCallback(orderServiceConfigData.getPaymentResponseTopicName(), paymentRequestAvroModel)
+        );
+
+    }
+
+    private ListenableFutureCallback<SendResult<String, PaymentRequestAvroModel>> kafkaCallback(String paymentResponseTopicName, PaymentRequestAvroModel paymentRequestAvroModel) {
+        return new ListenableFutureCallback<>() {
+            @Override
+            public void onFailure(Throwable ex) {
+                log.error("Error occurred when sending payment request to kafka topic", ex);
+
+            }
+            public void onSuccess(SendResult<String, PaymentRequestAvroModel> result) {
+                log.info("Successfully sent payment request to kafka topic");
+            }
+        };
+    }
+}
