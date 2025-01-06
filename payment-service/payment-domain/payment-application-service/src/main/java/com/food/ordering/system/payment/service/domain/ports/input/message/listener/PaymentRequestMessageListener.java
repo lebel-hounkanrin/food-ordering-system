d7055c2c0044f7cafe62b02d3c74e5ce4be62406
neil <lebelhounkanrin@gmail.com>
2025-01-06 10:20:10 +0100

    feat: implement persist payment method

--- payment-service/payment-domain/payment-application-service/src/main/java/com/food/ordering/system/payment/service/domain/ports/input/message/listener/PaymentRequestMessageListener.java
+++ payment-service/payment-domain/payment-application-service/src/main/java/com/food/ordering/system/payment/service/domain/ports/input/message/listener/PaymentRequestMessageListener.java
@@ -2,8 +2,8 @@ package com.food.ordering.system.payment.service.domain.ports.input.message.list
 
 import com.food.ordering.system.payment.service.domain.dto.PaymentRequest;
 
-public class PaymentRequestMessageListener {
-    void completePayment(PaymentRequest paymentRequest) {}
+public interface PaymentRequestMessageListener {
+    void completePayment(PaymentRequest paymentRequest);
 
-    void cancelPayment(PaymentRequest paymentRequest) {}
+    void cancelPayment(PaymentRequest paymentRequest);
 }
